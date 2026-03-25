d61200243328da19848afe4d432affb31c8586a6
Eerwig <er3r3r@protonmail.com>
2026-03-24 22:32:56 -0400

    Added uniforms

---
+++ uniforms/british.js
@@ -0,0 +1,4 @@
+api.giveItem(myId, "Light Gray Wood Helmet", 1);
+api.giveItem(myId, "Red Wood Chestplate", 1);
+api.giveItem(myId, "Black Wood Leggings", 1);
+api.giveItem(myId, "Brown Wood Boots", 1);
---
+++ uniforms/french.js
@@ -0,0 +1,4 @@
+api.giveItem(myId, "Gray Wood Helmet", 1);
+api.giveItem(myId, "Blue Wood Chestplate", 1);
+api.giveItem(myId, "White Wood Leggings", 1);
+api.giveItem(myId, "Black Wood Boots", 1);
