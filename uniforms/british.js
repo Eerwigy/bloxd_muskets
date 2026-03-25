8961828da1e3f19d4eebf2e5903a2214108f8feb
Eerwig <er3r3r@protonmail.com>
2026-03-25 18:50:45 -0400

    New uniform assignment code block

--- uniforms/british.js
+++
@@ -1,4 +0,0 @@
-api.giveItem(myId, "Gray Wood Helmet", 1);
-api.giveItem(myId, "Red Wood Chestplate", 1);
-api.giveItem(myId, "Light Gray Leggings", 1);
-api.giveItem(myId, "Black Wood Boots", 1);
--- uniforms/french.js
+++
@@ -1,4 +0,0 @@
-api.giveItem(myId, "Gray Wood Helmet", 1);
-api.giveItem(myId, "Blue Wood Chestplate", 1);
-api.giveItem(myId, "White Wood Leggings", 1);
-api.giveItem(myId, "Black Wood Boots", 1);
