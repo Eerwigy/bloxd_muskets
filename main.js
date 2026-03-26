4310606b480cf9247f6d48f9f53aa029812b52a4
Eerwig <er3r3r@protonmail.com>
2026-03-25 21:57:38 -0400

    Did some more cleanup

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const WEAPONS = {
   smoothbore: {
     speed: 1.5,
     damage: 2,
-    realoadSpeed: 10,
+    reloadSpeed: 10,
     loadedItem: "Wood Crossbow Charged",
     unloadedItem: "Wood Crossbow",
   },
@@ -10,42 +10,55 @@ const WEAPONS = {
   rifle: {
     speed: 3,
     damage: 4,
-    realoadSpeed: 8,
+    reloadSpeed: 8,
     loadedItem: "Stone Crossbow Charged",
     unloadedItem: "Stone Crossbow",
   },
 };
 
-var info = {
+const gameState = {
   gameStarted: false,
   players: {},
+  teams: {
+    british: [],
+    french: [],
+  },
 };
+
+function getPlayer(id) {
+  return gameState.players[id];
+}
 
 onPlayerJoin = (id) => {
-  info.players[id] = {
+  gameState.players[id] = {
     role: "regular",
-    currentQte: null,
+    team: "french",
     currentQteItem: null,
+    currentWeapon: null,
+    weaponSlot: null,
   };
+
+  gameState.teams.french.push(id);
 };
 
-onPlayerAttemptAltAction = (id, _x, _y, _z, blockName) => {
+onPlayerAttemptAltAction = (id) => {
   if (api.hasActiveQTE(id)) return "preventAction";
 
-  const player = info.players[id];
+  const player = getPlayer(id);
   if (!player) return "preventAction";
 
   const item = api.getHeldItem(id);
-  if (!item?.attributes?.customAttributes) return "preventAction";
+  const attrs = item?.attributes?.customAttributes;
 
-  const attrs = item.attributes.customAttributes;
+  if (!attrs) return "preventAction";
+
   const weaponName = attrs["muskets/name"];
   const weapon = WEAPONS[weaponName];
 
   if (!weapon) return "preventAction";
 
   if (attrs["muskets/loaded"]) {
-    fireMusket(id, item, attrs, weapon);
+    fireWeapon(id, item, attrs, weapon);
   } else {
     startReloadQTE(id, item, weapon);
   }
@@ -53,46 +66,40 @@ onPlayerAttemptAltAction = (id, _x, _y, _z, blockName) => {
   return "preventAction";
 };
 
-onPlayerFinishQTE = (id, qteid, succeed) => {
-  const player = info.players[id];
+onPlayerFinishQTE = (id, qteId, succeed) => {
+  const player = getPlayer(id);
   if (!player) return;
 
-  if (succeed && player.currentQteItem != null) {
+  if (succeed && player.currentQteItem) {
     const item = api.getItemSlot(id, player.weaponSlot);
     if (!item) return;
 
     const attrs = item.attributes?.customAttributes;
-    const weapon = player.currentWeapon;
+    if (!attrs) return;
 
-    if (attrs) {
-      attrs["muskets/loaded"] = true;
-      api.setItemSlot(
-        id,
-        player.weaponSlot,
-        weapon.loadedItem,
-        1,
-        item.attributes,
-        true,
-      );
-    }
+    attrs["muskets/loaded"] = true;
+
+    api.setItemSlot(
+      id,
+      player.weaponSlot,
+      player.currentWeapon.loadedItem,
+      1,
+      item.attributes,
+      true,
+    );
   }
 
-  player.currentQte = null;
   player.currentQteItem = null;
+  player.currentWeapon = null;
   player.weaponSlot = null;
 };
 
-function fireMusket(id, item, attrs, weapon) {
+function fireWeapon(id, item, attrs, weapon) {
+  const slot = api.getSelectedInventorySlotI(id);
+
   attrs["muskets/loaded"] = false;
 
-  api.setItemSlot(
-    id,
-    api.getSelectedInventorySlotI(id),
-    weapon.unloadedItem,
-    1,
-    item.attributes,
-    true,
-  );
+  api.setItemSlot(id, slot, weapon.unloadedItem, 1, item.attributes, true);
 
   const [x, y, z] = api.getPosition(id);
   const { dir } = api.getPlayerFacingInfo(id);
@@ -115,14 +122,14 @@ function fireMusket(id, item, attrs, weapon) {
 }
 
 function startReloadQTE(id, item, weapon) {
-  const player = info.players[id];
+  const player = getPlayer(id);
 
-  const qte = api.addQTE(id, {
+  const qteId = api.addQTE(id, {
     type: "progressBar",
     parameters: {
       progressStartValue: 10,
       progressDecreasePerTick: 0.5,
-      progressPerClick: weapon.realoadSpeed,
+      progressPerClick: weapon.reloadSpeed,
       canFail: true,
       description: [{ str: "Load your musket!" }],
       clickIcon: "fa-solid fa-computer-mouse",
@@ -131,8 +138,29 @@ function startReloadQTE(id, item, weapon) {
     },
   });
 
-  player.currentQte = qte;
   player.currentQteItem = item;
-  player.weaponSlot = api.getSelectedInventorySlotI(id);
   player.currentWeapon = weapon;
+  player.weaponSlot = api.getSelectedInventorySlotI(id);
+}
+
+function equipUniform(id) {
+  const i = gameState.players[id];
+
+  if (i.role === "regular") {
+    api.setItemSlot(id, 46, "Gray Wood Helmet", 1);
+  } else if (i.role === "captain") {
+    api.setItemSlot(id, 46, "Gold Helmet", 1);
+  } else if (i.role === "sharpshooter") {
+    api.setItemSlot(id, 46, "Green Wood Helmet", 1);
+  }
+
+  if (i.team === "british") {
+    api.setItemSlot(id, 47, "Red Wood Chestplate", 1);
+    api.setItemSlot(id, 48, "Light Gray Wood Leggings", 1);
+  } else if (i.team === "french") {
+    api.setItemSlot(id, 47, "Blue Wood Chestplate", 1);
+    api.setItemSlot(id, 49, "White Wood Leggings", 1);
+  }
+
+  api.setItemSlot(id, 50, "Black Wood Boots", 1);
 }
